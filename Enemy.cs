19c08f31e23b859553938371d1cb06af2c6b7972
PaulDom <54893606+PaulDom@users.noreply.github.com>
2023-05-14 12:25:07 +0300

    Add files via upload

--- Enemy.cs
+++ Enemy.cs
@@ -5,8 +5,10 @@ using UnityEngine;
 public class Enemy : MonoBehaviour
 {
     public Vector3 directionMove;
+    private float cornPosX;
     public float speed = 1;
     public int health = 4;
+    public Animator animator;
 
     public void TakeDamage()
     {
@@ -18,14 +20,23 @@ public class Enemy : MonoBehaviour
         }
     }
 
+    private void Start()
+    {
+        cornPosX = Corn.singleton.transform.position.x;
+    }
+
     private void Update()
     {
         float enemyPosX = transform.position.x;
-        float cornPosX = Corn.singleton.transform.position.x;
 
         if (enemyPosX > cornPosX)
         {
+            animator.SetBool("isMoving", true);
             transform.position += directionMove* speed * Time.deltaTime;
         } 
+        else
+        {
+            animator.SetBool("isMoving", false);
+        }
     }
 }
