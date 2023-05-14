53363d66391d022ccd616e369455bf5760fb032f
PaulDom <54893606+PaulDom@users.noreply.github.com>
2023-05-14 12:39:44 +0300

    Add files via upload

--- Corn.cs
+++ Corn.cs
@@ -5,9 +5,20 @@ using UnityEngine;
 public class Corn : MonoBehaviour
 {
     public static Corn singleton;
+    public int health = 100;
 
     private void Awake()
     {
         singleton = this;
     }
+
+    public void TakeDamage()
+    {
+        health -= 1;
+        if (health <= 0)
+        {
+            print("Кукурузу сьели");
+        }
+    }
+
 }
